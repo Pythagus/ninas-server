9fe286a08e0d3baf5bc0532a0569e22a8d1a4538
Pythagus <dmolina@etud.insa-toulouse.fr>
2022-02-22 16:28:43 +0100

    Remove useless console package

--- ninas/console.py
+++
@@ -1,9 +0,0 @@
-import colorama
-
-
-class Console(object):
-
-    @staticmethod
-    def error(message, code=42):
-        print(colorama.Fore.COLOR_RED + message + colorama.Fore.RESET)
-        exit(code)
